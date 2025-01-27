72dc37c45dd4445dbee2481a7e987ddc370d8a32
Courage Zeng <czeng@lcs.on.ca>
2025-01-27 09:22:47 -0500

    Initial project organization

---
+++ ToDoList/Model/ToDoItem.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by 曾梓恒 on 2025-01-27.
+//
+
+import Foundation
---
+++ ToDoList/Views/ItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemView.swift
+//  ToDoList
+//
+//  Created by 曾梓恒 on 2025-01-27.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemView()
+}
