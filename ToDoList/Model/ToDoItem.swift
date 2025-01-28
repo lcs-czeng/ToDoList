3674caa057734e959a2a3d508efe932200ca1e4e
Courage Zeng <czeng@lcs.on.ca>
2025-01-27 20:58:17 -0500

    Created structure to apply basic abstraction, DRY.

--- ToDoList/Model/ToDoItem.swift
+++ ToDoList/Model/ToDoItem.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct ToDoItem: Identifiable {
+    let id = UUID()
+    let itemName: String
+    let itemStatus: String
+}
+
+let Chemistry = ToDoItem(itemName: "Study for Chemistry quiz", itemStatus: "circle")
+
+let CompSci = ToDoItem(itemName: "Finish Computer Science assignment", itemStatus: "checkmark.circle")
+
+let Run = ToDoItem(itemName: "Go for a run around campus", itemStatus: "circle")
